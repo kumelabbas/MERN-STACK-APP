e700abb88c68bf61149cbe7af5203ec5c0bf3d66
kumelabbas <165946241+kumelabbas@users.noreply.github.com>
2024-06-01 13:07:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// Import required modules
+const express = require('express');
+//const { mongo, default: mongoose } = require('mongoose');
+const mongoose = require('mongoose')
+const bodyparser = require('body-parser');
+const ProductRoutes = require('./routes/index.js')
+const cors = require('cors');
+const app = express();
+
+app.use(bodyparser.json());
+app.use(cors());
+app.use(ProductRoutes)
+
+mongoose.connect("mongodb+srv://KUMEL:kumel1234@cluster0.eyadver.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{}).then(()=>{
+  console.log("Connected to Mognodb");
+}).catch((err)=>{
+    console.log("Error !", err);
+  })
+
+
+// Start the server
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
